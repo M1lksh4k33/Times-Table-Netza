10d0d04596025f67901d99af8641ff7772a72a5b
M1lksh4k33 <netza.baptichon@stu.ocsb.ca>
2024-12-10 15:56:32 +0000

    Times table netza

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 num= int(input("Enter a number: "))
 
 # initialize counter
-count= 0
+count= 10
 
-#multiply through the numbers from 10 to 20 with the user'snumber
+#multiply through the numbers from 10 to 20 with the user's number
 while(count<= 20):
   print(str(count) + "*" + str(num) + "=" + str(count * num))
   count= count + 1
