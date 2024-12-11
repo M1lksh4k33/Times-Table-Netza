aa8f05822c05f0f0db7c4eaf0578d83e44cdc1fb
M1lksh4k33 <netza.baptichon@stu.ocsb.ca>
2024-12-11 15:15:25 +0000

    While loops

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-# ask the user for a number
-num= int(input("Enter a number: "))
+# Ask user for positive integer
+user_input = int(input("Enter a positive integer: "))
 
-# initialize counter
-count= 10
+# Initialize variables
+sum_of_numbers= 0
+current_number= 0
 
-#multiply through the numbers from 10 to 20 with the user's number
-while(count<= 20):
-  print(str(count) + "*" + str(num) + "=" + str(count * num))
-  count= count + 1
+# Check if user input is positive
+if user_input<= 0:
+  print("Please enter a positive integer greater than zero.")
+else:
+  # Use a while loop to calculate the sum from 0 to the user input
+  while current_number <= user_input:
+    sum_of_numbers += current_number # Add the current number to the sum
+    current_number += 1 # Move to the next number
+
+    # Display the result
+    print(f"The sum of numbers from 0 to {user_input} is {sum_of_numbers}.")
